d493a0b641457b9f492e7fa5654c76ea9a303bab
zhouzedeng <729952708@qq.com>
2017-09-03 11:38:19 +0800

    [*]

--- Modules/Product/Http/routes.php
+++ Modules/Product/Http/routes.php
@@ -1,8 +1,8 @@
 <?php
 
-Route::group(['prefix' => 'open', 'namespace' => 'Modules\Product\Http\Controllers'], function()
+Route::group(['prefix' => 'api', 'namespace' => 'Modules\Product\Http\Controllers'], function()
 {
         Route::get('products/{comkey}', 'ProductController@index');
-        Route::get('products/{_product_id}', 'ProductController@show')->where('_product_id', '[0-9a-zA-Z]+');
+        Route::get('products/show/{_product_id}', 'ProductController@show')->where('_product_id', '[0-9a-zA-Z]+');
     
 });
